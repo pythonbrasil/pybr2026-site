2a91899a2c6d1109bdde6bb345a7ae6e71acf2f6
Talita Rossari <talitarossari@users.noreply.github.com>
2026-03-09 19:53:39 -0300

    Adicionando C4P (#5)
    * feat: Adicionando C4P
    
    * Resolvendo problema onde ao clicar nas opções do header em uma pagina que não fosse a home, o usuario não era levado para a seção selecionada. Utilizando HashLink
    opa MEOW
    
    * Corrigindo espaçamento dos botões
    MEOWWWWWWWW
    
    ---------
    
    Co-authored-by: Talita Rossari <talita.rossari@mercadolivre.com>
    Co-authored-by: Thiago Medeiros <thiagotitico1@gmail.com>

--- src/pages/home/section/Home.Section4.tsx
+++ src/pages/home/section/Home.Section4.tsx
@@ -6,14 +6,14 @@ import { useTranslation } from "react-i18next";
 export default function Home_Section4() {
     const { t } = useTranslation();
 
-    return(<section className="Home_Section4">
+    return(<section className="Home_Section4" id="SUBMISSAO_DE_ATIVIDADES">
             <div className="Home_Section4_itens_texto">
                 <h4>{t("home.section4.submissaoDeAtividades")}</h4>
                 <div className="Home_Section4_itens_texto_buttons">
-                    <a href=""><button className="Home_Section4_buttons_1">{t("home.section4.enviarAtividade")}</button></a>
-                    <a href=""><button className="Home_Section4_buttons_2">{t("home.section4.regulamento")}</button></a>
+                    <a href="https://talks.python.org.br/pybr26/cfp" target="_blank"><button className="Home_Section4_buttons_1">{t("home.section4.enviarAtividade")}</button></a>
+                    <a href="https://talks.python.org.br/pybr26/cfp" target="_blank"><button className="Home_Section4_buttons_2">{t("home.section4.regulamento")}</button></a>
                 </div>
-                <a href=""><p>{t("home.section4.paragrafo1")}</p></a>
+                <a href="https://instagram.com/pythonbrasil" target="_blank"><p>{t("home.section4.paragrafo1")}</p></a>
             </div>
             <div className="Home_Section4_cobras">
                 <img src={cobra_1} alt="" />
